32c0b179208887249523e9d23544479016c7136e
“github <github>
2024-10-21 18:04:59 +0900

    -

--- 자습/project-3/scr/script.js
+++ 자습/project-3/scr/script.js
@@ -3,57 +3,43 @@ const value = document.querySelectorAll('.result-area')
 const button = document.querySelectorAll('.button')
 const score = document.querySelector('.score')
 
-button.forEach((btn) => {
-   btn.addEventListener('click', () => {
-      // 카드 세팅
-      const cardNum = [Math.floor(Math.random() * 7) + 2]
-      let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      nums = nums.filter((n) => n !== cardNum[0])
-      cardNum.push(nums[Math.floor(Math.random() * nums.length)])
+const cardNum = [Math.floor(Math.random() * 7) + 2]
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+nums = nums.filter((n) => n !== cardNum[0])
+cardNum.push(nums[Math.floor(Math.random() * nums.length)])
 
-      // DOM 요소 가져오기
+cards[0].textContent = cardNum[0]
 
-      // 첫번째 카드 공개
-      cards[0].textContent = cardNum[0]
+const up = () => (cardNum[0] < cardNum[1] ? [true, '<'] : [false, '>'])
+const down = () => (cardNum[0] > cardNum[1] ? [true, '>'] : [false, '<'])
 
-      // up-down값을 비교
+function result(callback, name) {
+   for (let btn of button) btn.setAttribute('disabled', true)
+   name ? (button[0].className = 'button-color') : (button[1].className = 'button-color')
 
-      let eventStart = false
+   cards[1].style.transition = 'transform 0.5s'
+   cards[1].style.transform = 'rotateY(0deg)'
 
-      const up = () => (cardNum[0] < cardNum[1] ? [true, '<'] : [false, '>'])
-      const down = () => (cardNum[0] > cardNum[1] ? [true, '>'] : [false, '<'])
+   setTimeout(() => {
+      cards[1].textContent = cardNum[1]
+   }, 100)
 
-      function result(callback, name) {
-         for (let btn of button) btn.setAttribute('disabled', true)
-         name ? (button[0].className = 'button-color') : (button[1].className = 'button-color')
+   setTimeout(() => {
+      value[0].textContent = callback()[1]
+   }, 400)
 
-         cards[1].style.transition = 'transform 0.5s'
-         cards[1].style.transform = 'rotateY(0deg)'
+   setTimeout(() => {
+      callback()[0] ? (value[1].textContent = 'You Win!😉') : (value[1].textContent = 'Game Over😅')
+      value[1].style.transition = 'transform 0.5s'
+      value[1].style.transform = 'translateY(-10px)'
+   }, 800)
 
-         setTimeout(() => {
-            cards[1].textContent = cardNum[1]
-         }, 100)
-
-         setTimeout(() => {
-            value[0].textContent = callback()[1]
-         }, 400)
-
-         setTimeout(() => {
-            callback()[0]
-               ? (value[1].textContent = 'You Win!😉')
-               : (value[1].textContent = 'Game Over😅')
-            value[1].style.transition = 'transform 0.5s'
-            value[1].style.transform = 'translateY(-10px)'
-         }, 800)
-
-         setTimeout(() => {
-            value[2].innerHTML = `
+   setTimeout(() => {
+      value[2].innerHTML = `
                
                <button onClick="gameReplay()">재도전</button>
                `
-         }, 1500)
-      }
+   }, 1500)
+}
 
-      const gameReplay = () => (eventStart = false)
-   })
-})
+const gameReplay = () => (eventStart = false)
